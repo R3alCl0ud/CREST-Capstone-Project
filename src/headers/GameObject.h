00dc9960cc670414c0e4b28130e3bf8e630d6179
Perry Berman <perryberman.30@gmail.com>
2018-01-11 12:03:29 -0700

    Updated GameObject header

--- src/headers/GameObject.h
+++ src/headers/GameObject.h
@@ -7,13 +7,20 @@ class GameObject {
 public:
   GameObject() {
     this->position=sf::Vector2f(0,0);
+    this->physics2D=Physics2D();
+    this->rotation = 0;
   }
   void update();
   void start();
   Physics2D getPhysics();
   sf::Vector2f getPosition();
+  void setPosition();
+  float getRotation();
+  void setRotation();
 private:
   sf::Vector2f position;
+  Physics2D physics2D;
+  float rotation;
 };
 
 #endif
