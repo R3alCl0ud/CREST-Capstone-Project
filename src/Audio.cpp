b7f335e2348a500a570f90a1040738b726d52112
KaRaTeLoRd11 <zkozak@pvlearners.net>
2018-02-06 11:16:46 -0700

    added headers

--- src/Audio.cpp
+++ src/Audio.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "headers/Audio.hpp"
+#include <string.h>
 
 #include <SFML/Audio.hpp>
 #include "headers/Game.hpp"
 #include "headers/Level.hpp"
+#include "headers/Sound.hpp"
 
 namespace audio {
 
