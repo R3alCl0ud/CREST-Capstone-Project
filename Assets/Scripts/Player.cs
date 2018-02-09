c60438c9cfa75f90707ea15aa517f87548cc6530
Perry Berman <perryberman.30@gmail.com>
2018-02-09 12:05:18 -0700

    player controlls

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,17 +6,30 @@ public class Player : MonoBehaviour {
 
 
 	// public member fields
-	public GameObject camera;
+	public GameObject Camera;
+
+	// private member fields
+	private Rigidbody2D rb;
 
 	// public member methods
-	public void Start () {
-		
+	public void Start() {
+		rb = GetComponent<Rigidbody2D>();
 	}
 	
-	public void Update () {
-		if (camera) {
-			// camera.
-			camera.transform.SetPositionAndRotation(transform.position, Quaternion.identity);
+	public void Update() {
+		if (Camera) {
+			Vector3 pos = transform.position + new Vector3(0, 0, -10);
+			Camera.transform.SetPositionAndRotation(pos, Quaternion.identity);
+		}
+
+
+		if (Input.GetKey("a") || Input.GetKey("left")) {
+			Debug.Log("left");
+			rb.AddForce(new Vector2(-500.0f * Time.deltaTime, 0));
+		}
+		if (Input.GetKey("d") || Input.GetKey("right")) {
+			Debug.Log("right");
+			rb.AddForce(new Vector2(500.0f * Time.deltaTime, 0));
 		}
 	}
 
