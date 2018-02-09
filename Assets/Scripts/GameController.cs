d10c9cae01bbb28ac731737fbee09f4591a21ac1
Perry Berman <perryberman.30@gmail.com>
2018-02-09 11:32:00 -0700

    basic game stuff

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	// public member fields
+	public GameObject platform_pref, spike_pref;
+
+	// private member fields
+	private List<GameObject> platforms;
+
+	// protected member fields
+
+
+	// public member methods
+	public void Start () {
+		
+	}
+	
+	public void Update () {
+		
+	}
+
+
+
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+	// public member fields
+	public GameObject camera;
+
+	// public member methods
+	public void Start () {
+		
+	}
+	
+	public void Update () {
+		if (camera) {
+			// camera.
+			camera.transform.SetPositionAndRotation(transform.position, Quaternion.identity);
+		}
+	}
+
+}
