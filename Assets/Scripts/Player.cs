5e0edd53cfdbf4300cfb30908782f783d6100599
Perry Berman <perryberman.30@gmail.com>
2018-02-12 10:58:48 -0700

    More assets

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,18 +10,37 @@ public class Player : MonoBehaviour {
 
 	// private member fields
 	private Rigidbody2D rb;
+	private bool canJump = false;
+
 
 	// public member methods
 	public void Start() {
 		rb = GetComponent<Rigidbody2D>();
 	}
-	
+
+	public void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Platform") {
+			canJump = true;
+		}
+	}
+
+	public void OnCollisionExit2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Platform") {
+			canJump = false;
+		}
+	}
+
+
 	public void Update() {
 		if (Camera) {
 			Vector3 pos = transform.position + new Vector3(0, 0, -10);
 			Camera.transform.SetPositionAndRotation(pos, Quaternion.identity);
 		}
 
+		if ((Input.GetKey("w") || Input.GetKey("up")) && canJump) {
+			rb.AddForce(new Vector2(0, 350));
+			canJump = false;
+		}
 
 		if (Input.GetKey("a") || Input.GetKey("left")) {
 			Debug.Log("left");
